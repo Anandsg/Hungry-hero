e73542e4ad0d93e611d48edae73023cad612bd7b
Anandagadugin <gaduginanand984@gmail.com>
2023-04-08 11:10:31 +0530

    adding Restro with props

--- app.js
+++ app.js
@@ -28,46 +28,35 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li>About us</li>
+          <li>Items</li>
           <li>Contact</li>
-          <li>Cart</li>
+          <li className="nav-logo">
+            <div className="nav-logo">&#128722;</div>
+            {/* </li> </a> */}
+          </li>
         </ul>
       </div>
     </div>
   );
 };
-// const RestruarantCards = () => {
-//   return (
-//     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-//       <img
-//         className="res-logo"
-//         alt="res-logo"
-//         src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/d06b7fa84cb8f8193f60d4c2de172347"
-//       />
-//         <h4 className="res-name">Hotel Emphire</h4>
-//         <h5 className="res-subName">Snacks, fast food, Beverages</h5>
-//         <h5 className="res-subNameStar">4.1 ★</h5>
-//         <h5 className="res-price">₹ 400 FOR TWO</h5>
-//       </div>
-//   );
-// };
-const RestruarantCards = () => { 
+
+const RestruarantCards = (props) => {
+  console.log(props);
   return (
     <div className="res-card">
-      <img
-        className="res-image"
-        alt="res-image"
-        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/d06b7fa84cb8f8193f60d4c2de172347"
-      />
+      <img className="res-image" alt="res-image" src={props.resImage} />
       <div className="res-details">
-        <h4 className="res-name">Hotel Emphire</h4>
+       
+        <h4 className="res-name">{props.resName}</h4>
         <div className="res-rating">
           <i className="fa fa-star"></i>
-          <span>4.5 ★</span>
+          <span>{props.resRating}</span>
         </div>
-        <div className="res-cuisine">Snacks, fast food, Beverages</div>
+        <div className="res-cuisine">
+          {props.resCuisine} - {props.deliveryTime} min
+        </div>
         <div className="res-price">
-          <span>₹ 700 for two</span>
+          <span>{props.resPrice}</span>
           <button className="res-order">Order Online</button>
         </div>
       </div>
@@ -81,11 +70,38 @@ const Body = () => {
         <h3>Search</h3>
       </div>
       <div className="res-container">
-        <RestruarantCards />
-        <RestruarantCards />
-        <RestruarantCards />
-        <RestruarantCards />
-        
+        <RestruarantCards
+          resName="Hotel Empire"
+          resCuisine="Snacks, fast food, Beverages"
+          resPrice="₹ 800 for two"
+          resRating="4.5 ★"
+          resImage="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/d06b7fa84cb8f8193f60d4c2de172347"
+          deliveryTime="23"
+        />
+        <RestruarantCards
+          resName="Sunny's chicken wings!"
+          resCuisine="American, Fast food"
+          resPrice="₹ 1200 for two"
+          resRating="4.3 ★"
+          resImage="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/5ba4ead0cbe4e39a9ed6beda5b94e5f6"
+          deliveryTime="46"
+        />
+        <RestruarantCards
+          resName="Wow! Momo"
+          resCuisine="Tibetan, Healthy food, Asian"
+          resPrice="₹ 540 for two"
+          resRating="4.1 ★"
+          resImage="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/b5mmszz2vfpdwegpaw5z"
+          deliveryTime="17"
+        />
+        <RestruarantCards
+          resName="Shri udupi Hotel"
+          resCuisine="South indian, snacks"
+          resPrice="₹ 170 for two"
+          resRating="3.9 ★"
+          resImage="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/jxp8y1chnqljwqylpkov"
+          deliveryTime="32"
+        />
       </div>
     </div>
   );
