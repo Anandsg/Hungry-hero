567f07387602b80f8a9607f118afa285fa45d780
Anandagadugin <gaduginanand984@gmail.com>
2023-04-13 00:40:30 +0530

    Added search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,31 +4,62 @@ import RestruarantCards from "./RestruarantCards";
 import { useState } from "react";
 import resList from "../utils/mockData";
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
 const Body = () => {
-  
   const [listOfRestuarants, setlistOfRestuarants] = useState(resList);
+  const [searchText, setSearchText] = useState("");
 
   return (
-    <div className="body">
-      <div className="filter-btn">
-        <span
-          className="filter-icon" 
-          onClick={() => {
-            const filteredList = listOfRestuarants.filter(
-              (res) => res.data.avgRating > 4
-            );
-            setlistOfRestuarants(filteredList);
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
           }}
-        > 
-          Rating: 4.0+
-        </span>
+        />
+
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, listOfRestuarants);
+            setlistOfRestuarants(data);
+          }}
+        >
+          search
+        </button>
       </div>
-      <div className="res-container">
-        {listOfRestuarants.map((restaurant) => (
-          <RestruarantCards key={restaurant.data.id} resData={restaurant} />
-        ))}
+
+      <div className="body">
+        <div className="filter-btn">
+          <span
+            className="filter-icon"
+            onClick={() => {
+              const filteredList = listOfRestuarants.filter(
+                (res) => res.data.avgRating > 4
+              );
+              setlistOfRestuarants(filteredList);
+            }}
+          >
+            Rating: 4.0+
+          </span>
+        </div>
+
+        <div className="res-container">
+          {listOfRestuarants.map((restaurant) => (
+            <RestruarantCards key={restaurant.data.id} resData={restaurant} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
