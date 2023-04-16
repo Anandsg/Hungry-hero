e31d265cd0ba92f38220ccd9bc7da66472ed430e
Anandagadugin <gaduginanand984@gmail.com>
2023-04-16 13:15:01 +0530

    fetching cards with API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
-// import React, { useState } from "react";
 import React from "react";
 import RestruarantCards from "./RestruarantCards";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import resList from "../utils/mockData";
 
 function filterData(searchText, restaurants) {
@@ -14,6 +13,23 @@ const Body = () => {
   const [listOfRestuarants, setlistOfRestuarants] = useState(resList);
   const [searchText, setSearchText] = useState("");
 
+// useEffect is HOOk it a call back function, this will be called not immediately but whenever useEffect wants to be called
+// when we have empty dependecy array => once after the render it will be called
+// dependecy array => once after initial changes render + everytime after render (my searchText changes)
+
+useEffect (() => {
+  // Fetch APT
+  getRestaurants();
+}, []);
+
+async function getRestaurants () {
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+  const json = await data.json();
+  console.log(json);
+
+  //optional chaininng
+  setlistOfRestuarants(json?.data?.cards[2]?.data?.data?.cards);
+}
   return (
     <>
       <div className="search-container">
