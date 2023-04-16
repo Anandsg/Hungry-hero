697acd55b5ddb4a4268e911c13eb71f0cb9daf59
Anandagadugin <gaduginanand984@gmail.com>
2023-04-16 18:36:22 +0530

    Adding shimmer UI logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 import RestruarantCards from "./RestruarantCards";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
@@ -10,27 +11,34 @@ function filterData(searchText, restaurants) {
   return filterData;
 }
 const Body = () => {
-  const [listOfRestuarants, setlistOfRestuarants] = useState(resList);
+  // Making copy of a data
+  const [AlllistOfRestuarants, setAlllistOfRestuarants] = useState([]);
+  const [filteredlistOfRestuarants, setfilteredlistOfRestuarants] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-// useEffect is HOOk it a call back function, this will be called not immediately but whenever useEffect wants to be called
-// when we have empty dependecy array => once after the render it will be called
-// dependecy array => once after initial changes render + everytime after render (my searchText changes)
+  // useEffect is HOOk it a call back function, this will be called not immediately but whenever useEffect wants to be called
+  // when we have empty dependecy array => once after the render it will be called
+  // dependecy array => once after initial changes render + everytime after render (my searchText changes)
 
-useEffect (() => {
-  // Fetch APT
-  getRestaurants();
-}, []);
+  useEffect(() => {
+    // Fetch APT
+    getRestaurants();
+  }, []);
 
-async function getRestaurants () {
-  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
-  const json = await data.json();
-  console.log(json);
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
 
-  //optional chaininng
-  setlistOfRestuarants(json?.data?.cards[2]?.data?.data?.cards);
-}
-  return (
+    //optional chaininng
+    setAlllistOfRestuarants(json?.data?.cards[2]?.data?.data?.cards);
+    setfilteredlistOfRestuarants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+  return (AlllistOfRestuarants.length === 0) ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -46,8 +54,8 @@ async function getRestaurants () {
         <button
           className="search-btn"
           onClick={() => {
-            const data = filterData(searchText, listOfRestuarants);
-            setlistOfRestuarants(data);
+            const data = filterData(searchText, AlllistOfRestuarants);
+            setfilteredlistOfRestuarants(data);
           }}
         >
           search
@@ -59,10 +67,10 @@ async function getRestaurants () {
           <span
             className="filter-icon"
             onClick={() => {
-              const filteredList = listOfRestuarants.filter(
+              const filteredList = AlllistOfRestuarants.filter(
                 (res) => res.data.avgRating > 4
               );
-              setlistOfRestuarants(filteredList);
+              setfilteredlistOfRestuarants(filteredList);
             }}
           >
             Rating: 4.0+
@@ -70,10 +78,10 @@ async function getRestaurants () {
         </div>
 
         <div className="res-container">
-          {listOfRestuarants.map((restaurant) => (
+          {filteredlistOfRestuarants.map((restaurant) => (
             <RestruarantCards key={restaurant.data.id} resData={restaurant} />
-          ))} 
-        </div> 
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ src/components/Shimmer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shimmer = () => {
+    return <h1> Shimmer loading..... </h1>
+}
+
+export default Shimmer;
