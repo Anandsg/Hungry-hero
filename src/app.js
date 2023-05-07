a8fe3905de7ea3e0205815658c91572bebd25fe4
Anandagadugin <gaduginanand984@gmail.com>
2023-05-07 22:07:47 +0530

    changes in cards flex

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import Error from "./components/error";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Contact from "./components/Contact";
 import RestruarantCards from "./components/RestruarantCards";
--- src/components/RestruarantCards.js
+++ src/components/RestruarantCards.js
@@ -13,25 +13,28 @@ const RestruarantCards = (props) => {
     costForTwo,
   } = resData?.data;
   return (
-    <div className="w-56 p-2 m-4 shadow-md hover:shadow-xl hover:scale-105 hover:shadow-slate-200 ransition duration-500 ease-in-out">
+    <div className="w-56 p-2 shadow-md hover:shadow-xl hover:scale-105 hover:shadow-slate-200 ransition duration-500 ease-in-out">
       <img
         alt={resData.data.name}
         src={CDN_URL + resData.data.cloudinaryImageId}
       />
-      <div className="res-details">
+      <div className="res-details px-2">
         <h4 className="font-bold">{name}</h4>
         <div className="">
-          <span className="bg-green-500 text-white font-bold text-xs">★ {avgRating }</span>
+          <span className="bg-green-500 text-white font-bold text-xs">
+            ★ {avgRating}
+          </span>
         </div>
-        <div className="font-light">
-            {resData.data.cuisines.join(", ")} - {deliveryTime} min
-          </div>
-          <div className="res-price">
-            <span className="font-medium">₹{costForTwo / 100} For two</span>
-          </div>
+        <div className="font-light text-xs">
+          {resData.data.cuisines.join(", ")} - {deliveryTime} min
+        </div>
+        <div className="res-price">
+          <span className="font-normal text-sm">₹{costForTwo / 100} For two</span>
         </div>
       </div>
+    </div>
   );
 };
 
 export default RestruarantCards;
+
