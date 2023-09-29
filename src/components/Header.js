8de932239eef96de3cfa611e1390de31d50645f0
vasujhawar <vasujhawar2001@gmail.com>
2023-09-30 01:06:52 +0530

    responsive hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,66 +5,84 @@ import useOnline from "../utils/useOnline";
 import UserContext from "../utils/userContext";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingCart,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [isLoggedIn, setisLoggedIn] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Track whether the menu is open
 
   const isOnline = useOnline();
   const { user } = useContext(UserContext);
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(cartItems);
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Function to close the menu
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <div className="flex items-center justify-between bg-gradient-to-r h-20 shadow-md ">
-      <div>
-        <Link to="/">
-          <img data-testid="logo" className="h-20" src={LOGO_URL} />
-        </Link>
-      </div>
-      <span className="font-serif text-yellow-600">
-        <span className="text-1xl font-bold">
-          <span className="text-gradient-orange">Browse</span>
-        </span>{" "}
-        <span className="text-base font-semibold text-gray-500">Order</span>{" "}
-        <span className="text-1xl font-extrabold">
-          <span className="text-gradient-orange">Enjoy!</span>
-        </span>
-      </span>
-
-      <div className="nav-items">
-        <ul className="flex ml-52">
+    <div className="shadow-md">
+      <div className="container mx-auto py-4 px-4 md:flex md:justify-between md:items-center">
+        <div className="flex items-center justify-between">
           <Link to="/">
-            {" "}
-            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out px-4">
+            <img
+              data-testid="logo"
+              className="h-12 md:h-20"
+              src={LOGO_URL}
+              alt="Logo"
+            />
+          </Link>
+          <span className="ml-2 text-xl font-bold">
+            <span className="text-orange-500">Browse</span> Order{" "}
+            <span className="text-orange-500">Enjoy!</span>
+          </span>
+
+          <div className="flex md:space-x-6 mt-4 md:mt-0">
+            {/* Hamburger Menu Button */}
+            <button
+              onClick={toggleMenu}
+              className="md:hidden focus:outline-none"
+            >
+              {!isMenuOpen && (
+                <FontAwesomeIcon icon={faBars} className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        {/* Navigation Links (Hidden on Small Screens) */}
+        <ul className="hidden md:flex space-x-4">
+          <Link to="/">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
               Home
             </li>
           </Link>
           <Link to="/About">
-            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out px-4">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
               About
             </li>
           </Link>
           <Link to="/Contact">
-            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out px-4">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
               Contact
             </li>
           </Link>
-          {/* <Link to="/Instamart">
-            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out px-3">
-              {" "}
-              Instamart
-            </li>
-          </Link> */}
           <Link to="/Help">
-            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out px-3">
-              {" "}
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
               Help
             </li>
           </Link>
           <Link
             to="/Cart"
-            className="flex items-center hover:text-orange-400 transition-all duration-300 ease-in-out px-3"
+            className="flex items-center hover:text-orange-400 transition-all duration-300 ease-in-out"
           >
             <div className="relative">
               <FontAwesomeIcon icon={faShoppingCart} className="w-5 h-5" />
@@ -76,25 +94,67 @@ const Header = () => {
               </span>
             </div>
           </Link>
-        </ul> 
-      </div>
-      {/* <h3>{isOnline ? "🟢" : "🔴"}</h3> */}
+        </ul>
 
-      {isLoggedIn ? (
-        <button
-          className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-          onClick={() => setisLoggedIn(false)}
-        >
-          Login ⇦
-        </button>
-      ) : (
-        <button
-          className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2  rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-          onClick={() => setisLoggedIn(true)}
-        >
-          Logout
-        </button>
-      )}
+        {/* Menu for Small Screens */}
+        {isMenuOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-white z-10">
+            <div className="w-full h-full p-4 flex flex-col items-center">
+              <button
+                onClick={toggleMenu}
+                className="md:hidden focus:outline-none absolute top-4 right-4"
+              >
+                <FontAwesomeIcon icon={faTimes} className="w-6 h-6" />
+              </button>
+              <ul className="flex flex-col space-y-4 items-center">
+                <Link to="/" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Home
+                  </li>
+                </Link>
+                <Link to="/About" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    About
+                  </li>
+                </Link>
+                <Link to="/Contact" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Contact
+                  </li>
+                </Link>
+                <Link to="/Help" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Help
+                  </li>
+                </Link>
+                <Link to="/Cart" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Cart
+                  </li>
+                </Link>
+              </ul>
+
+              <div className="mt-8">
+                {isLoggedIn ? (
+                  <button
+                    className="text-lg font-medium shadow-md px-4 py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+                    onClick={() => setisLoggedIn(false)}
+                  >
+                    Login ⇦
+                  </button>
+                ) : (
+                  <button
+                    className="text-lg font-medium shadow-md px-4 py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+                    onClick={() => setisLoggedIn(true)}
+                  >
+                    Logout
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
