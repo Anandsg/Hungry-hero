81edee9186f97a1d2a3444db815d32ffadb943fa
shahidkhatrii <shahidkhatrixyz@gmail.com>
2023-10-01 22:23:41 +0530

    Added Quantity functionality to Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../utils/cartSlice";
+import {
+  clearCart,
+  removeItem,
+  increaseQuantity,
+  decreaseQuantity,
+} from "../utils/cartSlice";
 import { CDN_URL } from "../utils/constants";
 import React from "react";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(cartItems);
+  console.log(cartItems, "cart items");
   const dispatch = useDispatch();
   function HandleClearCart() {
     dispatch(clearCart());
   }
+
   return (
     <div className="md:w-3/5 w-[80%]  m-auto py-4 min-h-screen ">
       <div className="flex justify-between items-center mb-4">
@@ -23,30 +29,64 @@ const Cart = () => {
       </div>
       <div className="flex flex-col ">
         {cartItems.map((item) => {
-          return <CartItem key={item?.card?.info?.id} {...item?.card?.info} />;
+          return <CartItem key={item?.info?.id} {...item?.info} />;
         })}
       </div>
     </div>
   );
 };
-const CartItem = ({ name, imageId, price, description }) => {
+const CartItem = ({ id, name, imageId, price, description, quantity }) => {
+  const dispatch = useDispatch();
+  function HandleRemoveItem() {
+    dispatch(removeItem(id));
+  }
+  function HandleQuantityIncrease() {
+    dispatch(increaseQuantity(id));
+  }
+  function HandleQuantityDecrease() {
+    dispatch(decreaseQuantity(id));
+  }
   return (
-    <div className="my-2 border-b py-2 md:flex-row flex flex-col text-sm  gap-8">
+    <div className="my-2 border-b py-2 md:flex-row flex flex-col text-sm  gap-8 relative">
+      <button
+        className="absolute bottom-0 right-0 mt-2 mr-2 px-2 py-1  hover:bg-gray-200 rounded-full focus:outline-none"
+        onClick={() => HandleRemoveItem()}
+      >
+        X
+      </button>
       <img
         src={
           imageId
             ? CDN_URL + imageId
             : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/39cd5e4824e5c011ffaf56ddc39891e8"
         }
-        alt="" 
+        alt=""
         className="w-32 h-20 rounded object-cover"
       />
-      <div className="flex flex-col">
+      <div className="flex flex-col ">
         <span className="font-semibold">{name}</span>
         <span className="font-semibold">
-          &#8377;{!price ? "250" : price / 100}
+          &#8377;
+          {!price ? "250" : (price / 100) * quantity}
         </span>
         <span className="text-sm text-gray-500">{description}</span>
+        <div className="flex items-center space-x-2">
+          <button
+            id="decreaseQuantity"
+            className="px-2 py-1 bg-gray-200 hover:bg-gray-100 rounded-full focus:outline-none"
+            onClick={() => HandleQuantityDecrease()}
+          >
+            -
+          </button>
+          <span>{quantity}</span>
+          <button
+            id="increaseQuantity"
+            className="px-2 py-1 bg-gray-200 hover:bg-gray-100 rounded-full focus:outline-none"
+            onClick={() => HandleQuantityIncrease()}
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -8,10 +8,43 @@ const cartSlice = createSlice({
 
   reducers: {
     addItem: (state, action) => {
-      state.items.push(action.payload);
+      // console.log(state.items, "STATE.ITEMS");
+      const index = state.items.findIndex(
+        ({ info }) => info.id === action.payload.card.info.id
+      );
+      console.log(index, "Index");
+      if (index < 0) {
+        state.items.push({ ...action.payload.card });
+        state.items[state.items.length - 1].info.quantity = 1;
+      } else {
+        state.items[index].info.quantity++;
+      }
+
+      console.log(state.items.length, "this is cart payload");
+    },
+    increaseQuantity: (state, action) => {
+      const index = state.items.findIndex(
+        ({ info }) => info.id === action.payload
+      );
+      if (index >= 0) {
+        state.items[index].info.quantity++;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const index = state.items.findIndex(
+        ({ info }) => info.id === action.payload
+      );
+      if (index >= 0 && state.items[index].info.quantity > 1) {
+        state.items[index].info.quantity--;
+      }
     },
     removeItem: (state, action) => {
-      state.items.pop();
+      const index = state.items.findIndex(
+        ({ info }) => info.id === action.payload
+      );
+      if (index >= 0) {
+        state.items.splice(index, 1);
+      }
     },
     clearCart: (state) => {
       state.items = [];
@@ -19,5 +52,11 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export const {
+  addItem,
+  removeItem,
+  clearCart,
+  increaseQuantity,
+  decreaseQuantity,
+} = cartSlice.actions;
 export default cartSlice.reducer;
