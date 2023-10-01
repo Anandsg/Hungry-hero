b4696b7e8e1a82d0cd1214ca93450db1fc1e37c3
Roni Sarkar <ronichandrasarkar@gmail.com>
2023-10-01 13:26:47 +0530

    solved empty cart feedback

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../utils/cartSlice";
 import { CDN_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 import React from "react";
 
 const Cart = () => {
@@ -10,6 +11,7 @@ const Cart = () => {
   function HandleClearCart() {
     dispatch(clearCart());
   }
+
   return (
     <div className="md:w-3/5 w-[80%]  m-auto py-4 min-h-screen ">
       <div className="flex justify-between items-center mb-4">
@@ -21,11 +23,17 @@ const Cart = () => {
           Clear Cart
         </button>
       </div>
-      <div className="flex flex-col ">
-        {cartItems.map((item) => {
-          return <CartItem key={item?.card?.info?.id} {...item?.card?.info} />;
-        })}
-      </div>
+      {cartItems.length > 0 ? (
+        <div className="flex flex-col ">
+          {cartItems.map((item) => {
+            return (
+              <CartItem key={item?.card?.info?.id} {...item?.card?.info} />
+            );
+          })}
+        </div>
+      ) : (
+        <EmptyCart />
+      )}
     </div>
   );
 };
@@ -38,7 +46,7 @@ const CartItem = ({ name, imageId, price, description }) => {
             ? CDN_URL + imageId
             : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/39cd5e4824e5c011ffaf56ddc39891e8"
         }
-        alt="" 
+        alt=""
         className="w-32 h-20 rounded object-cover"
       />
       <div className="flex flex-col">
@@ -48,6 +56,19 @@ const CartItem = ({ name, imageId, price, description }) => {
         </span>
         <span className="text-sm text-gray-500">{description}</span>
       </div>
+    </div>
+  );
+};
+const EmptyCart = () => {
+  return (
+    <div className="flex flex-col items-center h-screen mt-20">
+      <h1 className="text-orange-600 text-1xl">Cart Is Empty</h1>
+      <Link to="/" className="mt-4">
+        {" "}
+        <button className="bg-white hover:bg-orange-300 text-gray-800 font-semibold py-2 px-4 border border-orange-300 rounded shadow">
+          Back To Home Page
+        </button>
+      </Link>
     </div>
   );
 };
