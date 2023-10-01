91195353320f3a47723e8d4c61dfb034af0e666e
Sumit Upadhyay <skumarupadhyay1@gmail.com>
2023-10-01 19:45:34 +0530

    CORS Error Has Been Resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,8 +20,8 @@ const Body = () => {
 
   async function getRestaurants() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
-    );
+      "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    ,);
     const json = await data.json();
     console.log(json);
 
