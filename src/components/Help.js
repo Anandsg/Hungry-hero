8089fee419204186281d689e88d73048e1d440da
Roni Sarkar <ronichandrasarkar@gmail.com>
2023-10-01 21:35:26 +0530

    solve issue open multiple questionnaire at a time

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import faqsJsonData from "../utils/help.json";
 
-const Section = ({ title, description, isVisible, setIsVisible }) => {
+const Section = ({ title, description, isVisible, onClick }) => {
   return (
     <div className="border border-separate p-2 m-2">
       <h3 className="font-serif font-bold">{title}</h3>
-      <button
-        onClick={() => setIsVisible(!isVisible)}
-        className="cursor-pointer underline text-sm"
-      >
+      <button onClick={onClick} className="cursor-pointer underline text-sm">
         {isVisible ? "Hide" : "Show"}
       </button>
       {isVisible && (
@@ -20,30 +17,26 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
 
 const Help = () => {
   const faqsData = faqsJsonData.faqs;
-  const initialVisibleSections = [];
-  for (let index = 0; index < faqsData.length; index++) {
-    initialVisibleSections.push(index === 0);
-  }
-  const [visibleSections, setVisibleSections] = useState(
-    initialVisibleSections
-  );
+  const [openSectionIndex, setOpenSectionIndex] = useState(0);
 
   const toggleSectionVisibility = (index) => {
-    const newVisibleSections = [...visibleSections];
-    newVisibleSections[index] = !newVisibleSections[index];
-    setVisibleSections(newVisibleSections);
+    if (openSectionIndex === index) {
+      setOpenSectionIndex(-1);
+    } else {
+      setOpenSectionIndex(index);
+    }
   };
 
   return (
     <div>
       <h1 className="text-center p-2 m-2 font-semibold">FAQs</h1>
-      {faqsJsonData.faqs.map((item, index) => (
+      {faqsData.map((item, index) => (
         <div key={item.id}>
           <Section
             title={item.title}
             description={item.description}
-            isVisible={visibleSections[index]}
-            setIsVisible={() => toggleSectionVisibility(index)}
+            isVisible={openSectionIndex === index}
+            onClick={() => toggleSectionVisibility(index)}
           />
         </div>
       ))}
