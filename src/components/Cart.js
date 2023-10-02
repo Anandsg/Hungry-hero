3c0e8cdebd02fb922adb50db789ac2258b301e8a
Anand gadagin <109084312+Anandsg@users.noreply.github.com>
2023-10-02 19:27:48 +0530

    Revert "Refactor: Hide clear cart button and cart count on empty cart"

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -19,25 +19,23 @@ const Cart = () => {
 
   return (
     <div className="md:w-3/5 w-[80%]  m-auto py-4 min-h-screen ">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-semibold">Cart - {cartItems.length}</h1>
+        <button
+          className="text-xs font-medium bg-orange-300 py-1 px-2 hover:bg-orange-200 transition-all duration-300 ease-in-out rounded"
+          onClick={() => HandleClearCart()}
+        >
+          Clear Cart
+        </button>
+      </div>
       {cartItems.length > 0 ? (
-        <>
-          <div className="flex justify-between items-center mb-4">
-            <h1 className="text-2xl font-semibold">Cart - {cartItems.length}</h1>
-            <button
-              className="text-xs font-medium bg-orange-300 py-1 px-2 hover:bg-orange-200 transition-all duration-300 ease-in-out rounded"
-              onClick={() => HandleClearCart()}
-            >
-              Clear Cart
-            </button>
-          </div>
-          <div className="flex flex-col ">
-            {cartItems.map((item) => {
-              return (
-                <CartItem key={item?.info?.id} {...item?.info} />
-              );
-            })}
-          </div>
-        </>
+        <div className="flex flex-col ">
+          {cartItems.map((item) => {
+            return (
+              <CartItem key={item?.info?.id} {...item?.info} />
+            );
+          })}
+        </div>
       ) : (
         <EmptyCart />
       )}
