69f3fdbf6384f2e3b9d36e558e252e71ca964e08
Anandagadugin <gaduginanand984@gmail.com>
2023-10-03 08:22:50 +0530

    cost for two fix

--- src/components/RestruarantCards.js
+++ src/components/RestruarantCards.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { CDN_URL } from "../utils/constants";
-import { AiOutlineHeart,AiFillHeart} from 'react-icons/ai';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { Link } from "react-router-dom";
 
 const RestruarantCards = (props) => {
-  const { resData ,id,favlist,onClickFav} = props;
-  const [isfav,setfav] = useState(favlist.indexOf(id)==-1);
+  const { resData, id, favlist, onClickFav } = props;
+  const [isfav, setfav] = useState(favlist.indexOf(id) == -1);
   const {
     cloudinaryImageId,
     name,
@@ -29,44 +29,44 @@ const RestruarantCards = (props) => {
           alt=""
           className=" rounded object-cover"
         />
-        <span className="absolute text-xl top-0.5 right-0.5 cursor-pointer"style={{color:!isfav?"red":"gray"}} onClick={()=>{setfav(!isfav);onClickFav(id)}}>
-            <AiFillHeart/>
+        <span className="absolute text-xl top-0.5 right-0.5 cursor-pointer" style={{ color: !isfav ? "red" : "gray" }} onClick={() => { setfav(!isfav); onClickFav(id) }}>
+          <AiFillHeart />
         </span>
       </div>
       <Link
         to={"/restaurants/" + id}
       >
-      <div className="res-details px-2">
-        <h4 className="font-medium text-base text-black">{name}</h4>
-        <span className="">{cuisines.join(", ")}</span>
-        <div className="flex justify-between items-center my-2 font-medium">
-          <div className="flex items-center gap-1 px-1 text-white bg-green-500 font-semibold">
-            <span className="text-[0.6rem]">&#9733;</span>
-            <span className="text-[0.6rem]">
-              {avgRating === "--" ? "4.2" : avgRating}
-            </span>
-          </div>
-          <div className="w-[3px] h-[3px] rounded-full bg-black"></div>
-          <span className="">{slaString}</span>
-          {/* <div className="font-light text-xs">
+        <div className="res-details px-2">
+          <h4 className="font-medium text-base text-black">{name}</h4>
+          <span className="">{cuisines.join(", ")}</span>
+          <div className="flex justify-between items-center my-2 font-medium">
+            <div className="flex items-center gap-1 px-1 text-white bg-green-500 font-semibold">
+              <span className="text-[0.6rem]">&#9733;</span>
+              <span className="text-[0.6rem]">
+                {avgRating === "--" ? "4.2" : avgRating}
+              </span>
+            </div>
+            {/* <div className="w-[3px] h-[3px] rounded-full bg-black"></div> */}
+            <span className="">{slaString}</span>
+            {/* <div className="font-light text-xs">
             {resData.data.cuisines.join(", ")} - {deliveryTime} min
           </div> */}
-          <div className="res-price">
-            <span className="text-xs">
-             ₹{costForTwo / 100} FOR TWO
+            <div className="res-price">
+              <span className="text-xs">
+                {costForTwo}
+              </span>
+            </div>
+          </div>
+          <div className="flex border-t pt-4 gap-2  font-semibold"></div>
+
+          <div className="flex" style={{ justifyContent: "space-between" }}>
+            <span className="text-[#a0522d] text-center">
+              {!aggregatedDiscountInfo?.shortDescriptionList[0]?.meta
+                ? "30% off | Use NEWFUD"
+                : aggregatedDiscountInfo?.shortDescriptionList[0]?.meta}
             </span>
           </div>
         </div>
-        <div className="flex border-t pt-4 gap-2  font-semibold"></div>
-        
-        <div className="flex" style={{justifyContent:"space-between"}}>
-          <span className="text-[#a0522d] text-center">
-            {!aggregatedDiscountInfo?.shortDescriptionList[0]?.meta
-              ? "30% off | Use NEWFUD"
-              : aggregatedDiscountInfo?.shortDescriptionList[0]?.meta}
-          </span>
-        </div>
-      </div>
       </Link>
     </div>
   );
