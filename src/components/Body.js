f51824ff2ba77b486249a8643025288bce0a037a
M Hamza Nadeem <nadeemmhamza@gmail.com>
2023-10-05 23:57:40 +0500

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -105,7 +105,7 @@ const Body = () => {
 
               <button
                 data-testid="search-btn"
-                className="text-xs font-medium shadow-md px-2 py-2 outline-none ml-0 right-10 rounded border border-gray-300 bg-orange-500 hover:border-gray-500 transition-all duration-200 ease-in-out text-white rounded-none"
+                className="text-xs font-medium shadow-md px-2 py-2 outline-none ml-0 md:mr-2 right-10 rounded border border-gray-300 bg-orange-500 hover:border-gray-500 transition-all duration-200 ease-in-out text-white rounded-none"
                 onClick={() => {
                   const data = filterData(searchText, AlllistOfRestuarants);
                   setfilteredlistOfRestuarants(data);
