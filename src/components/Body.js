25d0836c16f7d04e7c78a01070cba2b2af35fa0e
M Hamza Nadeem <nadeemmhamza@gmail.com>
2023-10-05 23:50:21 +0500

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -81,7 +81,7 @@ const Body = () => {
               The restaurant you're searching for doesn't exist.
             </h2>
             <button
-              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+              className="text-xs font-medium shadow-md px-2 py-2 outline-none ml-0 right-10 rounded border border-gray-300 bg-orange-500 hover:border-gray-500 transition-all duration-200 ease-in-out text-white rounded-none md:bg-white md:text-black"
               onClick={() => {
                 window.location.href = "/";
               }}
