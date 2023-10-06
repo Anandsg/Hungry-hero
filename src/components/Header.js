61e78167c378e1a43fdf697ef8848b9dd3a64e15
rohit <rhtry.tech@gmail.com>
2023-10-06 17:06:31 +0530

    fix : allignement, spacing issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import React, { useState, useContext, useEffect } from "react";
-import { LOGO_URL } from "../utils/constants";
-import { Link } from "react-router-dom";
+import React, {useState, useContext, useEffect} from "react";
+import {LOGO_URL} from "../utils/constants";
+import {Link} from "react-router-dom";
 import useOnline from "../utils/useOnline";
 import UserContext from "../utils/userContext";
-import { useSelector } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {useSelector} from "react-redux";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
   faShoppingCart,
   faBars,
@@ -12,14 +12,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import ScrollToTop from "./ScrollToTop";
-import { useGoogleLogin } from "@react-oauth/google";
+import {useGoogleLogin} from "@react-oauth/google";
 
 const Header = () => {
   const [isLoggedIn, setisLoggedIn] = useState(true);
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Track whether the menu is open
 
   const isOnline = useOnline();
-  const { user } = useContext(UserContext);
+  const {user} = useContext(UserContext);
   const cartItems = useSelector((store) => store.cart.items);
 
   //Function to get userInfo
@@ -29,7 +29,7 @@ const Header = () => {
       Authorization: `Bearer ${access_token}`,
     };
     try {
-      const response = await fetch(url, { headers });
+      const response = await fetch(url, {headers});
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
@@ -71,14 +71,14 @@ const Header = () => {
   };
 
   const headerStyle = {
-    position: 'sticky',
-    top: '0',      // Stick to the top of the viewport
-    backgroundColor: 'white',
-    zIndex: '100', // Set a z-index to control stacking order  
+    position: "sticky",
+    top: "0", // Stick to the top of the viewport
+    backgroundColor: "white",
+    zIndex: "100", // Set a z-index to control stacking order
   };
 
   return (
-    <div className="shadow-md" style={headerStyle}>
+    <div className="shadow-md lg:px-36" style={headerStyle}>
       <div className="container mx-auto py-1 px-4 md:flex md:justify-between md:items-center">
         <div className="flex items-center justify-between">
           <Link to="/">
@@ -118,136 +118,136 @@ const Header = () => {
             <span className="text-orange-500">Enjoy!</span>
           </span> */}
 
-            <div className="flex md:space-x-6 mt-4 md:mt-0">
-              {/* Hamburger Menu Button */}
+          <div className="flex md:space-x-6">
+            {/* Hamburger Menu Button */}
+            <button
+              onClick={toggleMenu}
+              className="md:hidden focus:outline-none"
+            >
+              {!isMenuOpen && (
+                <FontAwesomeIcon icon={faBars} className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        {/* Navigation Links (Hidden on Small Screens) */}
+        <ul className="hidden md:flex items-center space-x-4">
+          <Link to="/">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+              Home
+            </li>
+          </Link>
+          <Link to="/About">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+              About
+            </li>
+          </Link>
+          <Link to="/Contact">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+              Contact
+            </li>
+          </Link>
+          <Link to="/Help">
+            <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+              Help
+            </li>
+          </Link>
+          <Link
+            to="/Cart"
+            className="flex items-center hover:text-orange-400 transition-all duration-300 ease-in-out"
+          >
+            <div className="relative">
+              <FontAwesomeIcon icon={faShoppingCart} className="w-5 h-5" />
+              <span
+                className="absolute top-[-20%] right-[-32%] inline-flex items-center justify-center w-3 h-3.5 bg-orange-500 text-white rounded-full text-xs"
+                data-testid="cart"
+              >
+                {cartItems.length}
+              </span>
+            </div>
+          </Link>
+
+          {/* Will enable login feature once i setup google client ID */}
+
+          {isLoggedIn ? (
+            <button
+              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+              onClick={() => {
+                login();
+              }}
+            >
+              Login ⇦
+            </button>
+          ) : (
+            <button
+              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+              onClick={() => logout()}
+            >
+              Logout
+            </button>
+          )}
+        </ul>
+
+        {/* Menu for Small Screens */}
+        {isMenuOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-white z-10">
+            <div className="w-full h-full p-4 flex flex-col items-center">
               <button
                 onClick={toggleMenu}
-                className="md:hidden focus:outline-none"
+                className="md:hidden focus:outline-none absolute top-4 right-4"
               >
-                {!isMenuOpen && (
-                  <FontAwesomeIcon icon={faBars} className="w-6 h-6" />
-                )}
+                <FontAwesomeIcon icon={faTimes} className="w-6 h-6" />
               </button>
+              <ul className="flex flex-col space-y-4 items-center">
+                <Link to="/" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Home
+                  </li>
+                </Link>
+                <Link to="/About" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    About
+                  </li>
+                </Link>
+                <Link to="/Contact" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Contact
+                  </li>
+                </Link>
+                <Link to="/Help" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Help
+                  </li>
+                </Link>
+                <Link to="/Cart" onClick={closeMenu}>
+                  <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
+                    Cart
+                  </li>
+                </Link>
+
+                {isLoggedIn ? (
+                  <button
+                    className="w-24 text-lg font-medium shadow-md py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+                    onClick={() => {
+                      login();
+                    }}
+                  >
+                    Login ⇦
+                  </button>
+                ) : (
+                  <button
+                    className="w-24 text-lg font-medium shadow-md py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+                    onClick={() => logout()}
+                  >
+                    Logout
+                  </button>
+                )}
+              </ul>
             </div>
           </div>
-          {/* Navigation Links (Hidden on Small Screens) */}
-          <ul className="hidden md:flex items-center space-x-4">
-            <Link to="/">
-              <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                Home
-              </li>
-            </Link>
-            <Link to="/About">
-              <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                About
-              </li>
-            </Link>
-            <Link to="/Contact">
-              <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                Contact
-              </li>
-            </Link>
-            <Link to="/Help">
-              <li className="hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                Help
-              </li>
-            </Link>
-            <Link
-              to="/Cart"
-              className="flex items-center hover:text-orange-400 transition-all duration-300 ease-in-out"
-            >
-              <div className="relative">
-                <FontAwesomeIcon icon={faShoppingCart} className="w-5 h-5" />
-                <span
-                  className="absolute top-[-20%] right-[-32%] inline-flex items-center justify-center w-3 h-3.5 bg-orange-500 text-white rounded-full text-xs"
-                  data-testid="cart"
-                >
-                  {cartItems.length}
-                </span>
-              </div>
-            </Link>
-
-            {/* Will enable login feature once i setup google client ID */}
-
-            {isLoggedIn ? (
-              <button
-                className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-                onClick={() => {
-                  login();
-                }}
-              >
-                Login ⇦
-              </button>
-            ) : (
-              <button
-                className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-                onClick={() => logout()}
-              >
-                Logout
-              </button>
-            )}
-          </ul>
-
-          {/* Menu for Small Screens */}
-          {isMenuOpen && (
-            <div className="fixed inset-0 flex items-center justify-center bg-white z-10">
-              <div className="w-full h-full p-4 flex flex-col items-center">
-                <button
-                  onClick={toggleMenu}
-                  className="md:hidden focus:outline-none absolute top-4 right-4"
-                >
-                  <FontAwesomeIcon icon={faTimes} className="w-6 h-6" />
-                </button>
-                <ul className="flex flex-col space-y-4 items-center">
-                  <Link to="/" onClick={closeMenu}>
-                    <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                      Home
-                    </li>
-                  </Link>
-                  <Link to="/About" onClick={closeMenu}>
-                    <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                      About
-                    </li>
-                  </Link>
-                  <Link to="/Contact" onClick={closeMenu}>
-                    <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                      Contact
-                    </li>
-                  </Link>
-                  <Link to="/Help" onClick={closeMenu}>
-                    <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                      Help
-                    </li>
-                  </Link>
-                  <Link to="/Cart" onClick={closeMenu}>
-                    <li className="text-2xl text-gray-800 hover:text-orange-400 transition-all font-serif duration-300 ease-in-out">
-                      Cart
-                    </li>
-                  </Link>
-
-                  {isLoggedIn ? (
-                    <button
-                      className="w-24 text-lg font-medium shadow-md py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-                      onClick={() => {
-                        login();
-                      }}
-                    >
-                      Login ⇦
-                    </button>
-                  ) : (
-                    <button
-                      className="w-24 text-lg font-medium shadow-md py-2 outline-none m-2 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
-                      onClick={() => logout()}
-                    >
-                      Logout
-                    </button>
-                  )}
-                </ul>
-              </div>
-            </div>
-          )}
-        </div>
+        )}
       </div>
+    </div>
     // </>
   );
 };
--- src/components/RestruarantCards.js
+++ src/components/RestruarantCards.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { CDN_URL } from "../utils/constants";
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import {CDN_URL} from "../utils/constants";
+import {AiOutlineHeart, AiFillHeart} from "react-icons/ai";
+import {Link} from "react-router-dom";
 
 const RestruarantCards = (props) => {
-  const { resData, id, favlist, onClickFav } = props;
+  const {resData, id, favlist, onClickFav} = props;
   const [isfav, setfav] = useState(favlist.indexOf(id) == -1);
   const {
     cloudinaryImageId,
@@ -17,7 +17,7 @@ const RestruarantCards = (props) => {
     aggregatedDiscountInfo,
   } = resData;
   return (
-    <div className="md:w-60 shadow-md md:shadow-none py-4 px-4 md:py-2  hover:shadow-xl rounded flex flex-col gap-1 text-[0.7rem] text-[#535665] " >
+    <div className="md:w-60 lg:w-full shadow-md md:shadow-none p-4 md:py-2 hover:shadow-xl rounded flex flex-col gap-1 text-[0.7rem] text-[#535665] ">
       <div className="relative">
         <img
           src={
@@ -29,13 +29,19 @@ const RestruarantCards = (props) => {
           alt=""
           className=" rounded object-cover"
         />
-        <span className={ `absolute text-xl top-0.5 right-0.5 cursor-pointer ${!isfav ? "text-[red]" : "text-[gray] hover:text-[red] duration-200" }`  } onClick={() => { setfav(!isfav); onClickFav(id) }}>
+        <span
+          className={`absolute text-xl top-0.5 right-0.5 cursor-pointer ${
+            !isfav ? "text-[red]" : "text-[gray] hover:text-[red] duration-200"
+          }`}
+          onClick={() => {
+            setfav(!isfav);
+            onClickFav(id);
+          }}
+        >
           <AiFillHeart />
         </span>
       </div>
-      <Link
-        to={"/restaurants/" + id}
-      >
+      <Link to={"/restaurants/" + id}>
         <div className="res-details px-2">
           <h4 className="font-medium text-base text-black">{name}</h4>
           <span className="text-[0.8rem]">{cuisines.join(", ")}</span>
@@ -52,14 +58,12 @@ const RestruarantCards = (props) => {
             {resData.data.cuisines.join(", ")} - {deliveryTime} min
           </div> */}
             <div className="res-price">
-              <span className="text-xs">
-                {costForTwo}
-              </span>
+              <span className="text-xs">{costForTwo}</span>
             </div>
           </div>
           <div className="flex border-t pt-4 gap-2  font-semibold"></div>
 
-          <div className="flex" style={{ justifyContent: "space-between" }}>
+          <div className="flex" style={{justifyContent: "space-between"}}>
             <span className="text-[#a0522d] text-center">
               {!aggregatedDiscountInfo?.shortDescriptionList[0]?.meta
                 ? "30% off | Use NEWFUD"
