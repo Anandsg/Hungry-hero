da4141bf942135d64cc0de9c0c71b2ad25c8610d
psb27 <singhprateek2704@gmail.com>
2023-10-06 18:20:50 +0530

    enhance user experience non existing page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import ErrorImage from "../assets/404-Error.png";
+import { Link } from "react-router-dom"
+import Body from "./Body";
+import Main from "./Main";
 
 const Error = () => {
   const err = useRouteError();
@@ -7,8 +11,11 @@ const Error = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-500 mb-4">Oops!</h1>
-      <h2 className="text-2xl text-gray-800">there appears to be a glitch.</h2>
+      <img className="max-w-sm h-auto justify-center object-center my-1.5" src={ErrorImage} alt="Error 404 Not Found" />
+      <h1 className="text-2xl font-bold text-red-500 my-1.5">Oops!</h1>
+      <h2 className="text-xl my-1.5">The restaurant you are looking for does not exist.</h2>
+      <Link className=" my-1.5 bg-white hover:bg-orange-300 text-gray-800 font-semibold py-2 px-4 border border-orange-300 rounded shadow"  to="/">Return Home</Link>
+         
     </div>
   );
 };
