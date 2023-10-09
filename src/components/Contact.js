1c3f282e4c97a88e04d1f6dd58a1e2788db69bf0
Rahul Roy <rroy1836@gmail.com>
2023-10-09 14:07:28 +0530

    Fixes Logo and Menu Page Dark Mode

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -110,14 +110,14 @@ const Contact = () => {
           <div className="flex justify-around">
             <button
               type="reset"
-              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer md:bg-white md:text-black"
             >
               Clear 
             </button>
             <input
               type="submit"
               value="Submit"
-              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer"
+              className="text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-gray-700 cursor-pointer md:bg-white md:text-black"
               onClick={handleSubmitButton}
             ></input>
           </div>
