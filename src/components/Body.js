5ead1d5b3e9f8a3c43e4e0cd3e1e74755ba00a09
unknown <shiveshtiwari0@gmail.com>
2023-10-14 23:25:02 +0530

    fixed the not found images

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { filterData } from "../utils/helper";
 import { GrNotification } from "react-icons/gr";
-import { ImSad } from "react-icons/im";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import EmptyFavTab from "../assets/empty-plate.jpg";
+import RamenRestaurant from "../assets/ramen-restaurant.jpg";
 import ArrowIcon from "../assets/arrow-icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useFocus } from "../utils/useFocus";
@@ -162,8 +162,8 @@ const Body = () => {
             <div className="flex items-center  mt-2  md:mt-0">
               <span
                 className={`text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-black cursor-pointer ${showFitler
-                    ? "border-orange-300 text-orange-300 hover:border-orange-500"
-                    : ""
+                  ? "border-orange-300 text-orange-300 hover:border-orange-500"
+                  : ""
                   }`}
                 onClick={() => {
                   let filteredList = listOfRestaurants;
@@ -181,8 +181,8 @@ const Body = () => {
               </span>
               <span
                 className={`text-xs font-medium shadow-md px-2 py-2 outline-none m-2 right-10 rounded border border-gray-300 hover:border-gray-500 transition-all duration-200 ease-in-out text-black cursor-pointer ${showFav
-                    ? "border-orange-300 text-orange-300 hover:border-orange-500"
-                    : ""
+                  ? "border-orange-300 text-orange-300 hover:border-orange-500"
+                  : ""
                   }`}
                 onClick={() => {
                   let favouriteList = listOfRestaurants;
@@ -205,11 +205,15 @@ const Body = () => {
 
         {filteredListOfRestaurants?.length === 0 && searchText !== "" && (
           <div className="flex flex-col items-center">
-            <ImSad size={100} className="mt-8" />
+            <img src={RamenRestaurant} alt="icon" className="mt-8 h-64" />
 
-            <h2 className="font-bold text-center mt-12">
+            <h1 className="text-lg font-bold">
+              Oops!
+            </h1>
+
+            <p className="text-center">
               The restaurant you're searching for doesn't exist.
-            </h2>
+            </p>
 
             <button
               className="text-xs font-medium shadow-md px-2 py-2 outline-none ml-0 right-10 border border-gray-300 bg-orange-500 hover:border-gray-500 transition-all duration-200 ease-in-out text-white rounded-none mt-4"
