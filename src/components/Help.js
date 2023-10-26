39981c6072dcb05b43901ec72e54aac7458b7922
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-10-26 12:08:50 +0530

    added the ui improvement in the FAQs

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import faqsJsonData from "../utils/help.json";
 
@@ -8,14 +13,22 @@ const Section = ({ title, description, isVisible, onClick }) => {
     <Accordion
       expanded={isVisible}
       onChange={onClick}
-      style={{ borderRadius: '1rem', transition: 'ease-in-out .13s', boxShadow: 'rgba(17, 17, 26, 0.1) 0px 0px 16px' }}
-      className="border rounded-2xl m-4 overflow-hidden transition-all transform hover:scale-105 duration-300 ease-in"
+      style={{
+        borderRadius: "1rem",
+        transition: "ease-in-out .13s",
+        boxShadow: "rgba(17, 17, 26, 0.1) 0px 0px 16px",
+      }}
+      className="border rounded-2xl m-4 overflow-hidden transition-all transform hover:scale-105  duration-300 ease-in"
     >
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         className="bg-blue-200 transition-all rounded-t-2xl"
       >
-        <Typography variant="h6" style={{ fontSize: '1.25rem',fontWeight:'500' }} className="text-gray-800 hover:text-gray-900">
+        <Typography
+          variant="h6"
+          style={{ fontSize: "1.25rem", fontWeight: "500" }}
+          className="text-gray-800 hover:text-orange-400"
+        >
           {title}
         </Typography>
       </AccordionSummary>
@@ -36,7 +49,15 @@ const Help = () => {
 
   return (
     <div className="container mx-auto p-8">
-      <Typography variant="h4" align="center" className="text-gray-900 mb-8">
+      <Typography
+        variant="h4"
+        align="center"
+        className="text-gray-900"
+        style={{
+          fontWeight: "600",
+          fontSize: "2.5rem",
+        }}
+      >
         FAQs
       </Typography>
       {faqsData.map((item, index) => (
