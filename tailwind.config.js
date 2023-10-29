4e6d49a3b917f2aa7c081e8ca1cd64f1f3fd2fb2
Abhishek Subba <abhisheksubba.py@gmail.com>
2023-10-29 20:26:00 +0530

    add wiggle anim to heart

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
